0661ef80ce3f45ddb41f01d2a3f6fb74cbb352a8
Muhammad Anas <muhd.anas@outlook.com>
2024-07-23 13:51:15 +0800

    translation vector calculations

--- src/bumperbot_cpp_examples/src/simple_turtlesim_kinematics.cpp
+++ src/bumperbot_cpp_examples/src/simple_turtlesim_kinematics.cpp
@@ -1,3 +1,37 @@
-//
-// Created by muhda on 22/7/2024.
-//
+#include "../include/bumperbot_cpp_examples/simple_turtlesim_kinematics.hpp"
+#include <turtlesim/msg/pose.hpp>
+
+using std::placeholders::_1;
+
+SimpleTurtlesimKinematics::SimpleTurtlesimKinematics(const std::string &name) :Node(name)
+{
+    turtle1_pose_sub_ = create_subscription<turtlesim::msg::Pose>("/turtle1/pose", 10,
+                                                                  std::bind(&SimpleTurtlesimKinematics::turtle1PoseCallback,this, _1));
+    turtle2_pose_sub_ = create_subscription<turtlesim::msg::Pose>("/turtle2/pose", 10,
+                                                                  std::bind(&SimpleTurtlesimKinematics::turtle2PoseCallback,this, _1));
+}
+
+void SimpleTurtlesimKinematics::turtle1PoseCallback(const turtlesim::msg::Pose &pose)
+{
+    last_turtle1_pose_ = pose;
+}
+
+void SimpleTurtlesimKinematics::turtle2PoseCallback(const turtlesim::msg::Pose &pose)
+{
+    last_turtle2_pose_ = pose;
+
+    float Tx =  last_turtle2_pose_.x - last_turtle1_pose_.x;
+    float Ty =  last_turtle2_pose_.y - last_turtle1_pose_.y;
+
+    RCLCPP_INFO_STREAM(get_logger(), "\n T vector turtle1 -> turtle2 \n" << "Tx: " << Tx << "\n" << "Ty: " << Ty << "\n" );
+}
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SimpleTurtlesimKinematics>("simple_turtlesim_kinematics");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+
+    return 0;
+}

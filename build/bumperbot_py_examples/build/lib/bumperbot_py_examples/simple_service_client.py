5c3b7c518d5427d4ebee21b82e7734914cfce67c
Muhammad Anas <muhd.anas@outlook.com>
2024-07-29 10:33:01 +0800

    add simple service server implementation

---
+++ build/bumperbot_py_examples/build/lib/bumperbot_py_examples/simple_service_client.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+from bumperbot_msgs.srv import AddTwoInts
+import sys
+class SimpleServiceClient(Node):
+    def __init__(self, a, b):
+        super().__init__('simple_service_client')
+        self.client_ = self.create_client(AddTwoInts, 'add_two_ints')
+
+        while not self.client_.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+
+        self.req = AddTwoInts.Request()
+        self.req.a = a
+        self.req.b = b
+        self.future_ = self.client_.call_async(self.req)
+        self.future_.add_done_callback(self.responseCallback)
+
+    def responseCallback(self, future):
+        self.get_logger().info('future result: %d', future.result().sum)
+
+def main(args=None):
+    rclpy.init(args=args)
+    if len(sys.argv) != 3:
+        print("Wrong number of arguments, usage: python simple_service_client.py <a> <b>")
+        return -1
+    simple_service_client = SimpleServiceClient(sys.argv[1], int(sys.argv[2]))
+    rclpy.spin(simple_service_client)
+    simple_service_client.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
